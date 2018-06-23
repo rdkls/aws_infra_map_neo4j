92d4788df3b5dceeef403778277e6204eb92750e
nick <nick.p.doyle@gmail.com>
2018-06-24 03:30:47 +1000

    entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 neo4j start
-while sleep 1 ; do
+no_success=true
+while $no_success ; do
+    sleep 1
+    echo "waiting for neo4j to start ..."
     if [[ `tail -n2 /var/lib/neo4j/logs/neo4j.log | grep 'Remote interface available'` ]] ; then
+        no_success=false
         /bin/bash
     fi
-    echo "waiting for neo4j to start ..."
 done
