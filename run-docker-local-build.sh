fb2cb3d15fe07ff5cf9866ca08c52a87be2605f7
Nick Doyle <nick.doyle@intelematics.com>
2020-05-07 15:54:53 +1000

    feat: move limit to region var

--- run-docker-local-build.sh
+++ run-docker-local-build.sh
@@ -7,18 +7,23 @@ if [ -z "$AWS_SECRET_ACCESS_KEY" ] ; then
 	AWS_ACCESS_KEY_ID=`grep aws_access_key_id ~/.aws/credentials | head -n1 | cut -d'=' -f 2 | sed 's/ //g'`
 	AWS_SESSION_TOKEN=`grep aws_session_token ~/.aws/credentials | head -n1 | cut -d'=' -f 2 | sed 's/ //g'`
 	AWS_SECURITY_TOKEN=`grep aws_security_token ~/.aws/credentials | head -n1 | cut -d'=' -f 2 | sed 's/ //g'`
+fi
+if [ -z "$AWS_REGION" ] ; then
 	AWS_REGION=`grep region ~/.aws/config | head -n1 | cut -d'=' -f 2 | sed 's/ //g'`
 fi
 
 docker rm -f neo4j_awless 2>/dev/null
 docker build --tag aws_infra_map_neo4j:latest .
 
+export AWS_TO_NEO4J_LIMIT_REGION=ap-southeast-2
+
 docker run -ti \
     --name neo4j_awless \
     --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     --env AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     --env AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN \
-    --env AWS_TO_NEO4J_LIMIT_REGION=ap-southeast-2  \
+    --env AWS_DEFAULT_REGION=$AWS_REGION \
+	--env AWS_TO_NEO4J_LIMIT_REGION=$AWS_TO_NEO4J_LIMIT_REGION \
     --env NEO4J_AUTH=$NEO4J_AUTH \
     -p 7474:7474 \
     -p 7687:7687 \
